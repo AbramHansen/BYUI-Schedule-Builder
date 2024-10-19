7f248cbaf72e4334b37099c9258c7f8cd8dd7321
Abram Hansen <hansenabram@gmail.com>
2024-10-18 21:14:31 -0600

    added test sections and courses

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
 from section import Section, DeliveryMethod, Block
 
 def test():
-    testSection = []
+    cppSections = []
+    dataStructuresSections = []
+    psycologySections = []
+    programmingWithClassesSections = []
 
-    testSection.append(Section([],'CSE 220C', 'A1', DeliveryMethod.Online, Block.Second, "C++ Language", 1))
-    testSection.append(Section([
-        ('T',14.5,16), ('R',14.5,16)
+    cppSections.append(Section([],'CSE 220C', 'A1', DeliveryMethod.Online, Block.Second, "C++ Language", 1))
+    cppSections.append(Section([('W',13.25,14.25)],'CSE 220C', 'A2', DeliveryMethod.InPerson, Block.Second, "C++ Language", 1))
+    cppSections.append(Section([('W',13.25,14.25)],'CSE 220C', 'A3', DeliveryMethod.InPerson, Block.Second, "C++ Language", 1))
+    cppSections.append(Section([],'CSE 220C', 'A4', DeliveryMethod.Online, Block.Second, "C++ Language", 1))
+
+    dataStructuresSections.append(Section([
+        ('M',14.5,16), ('W',14.5,16)
     ],'CSE 336', 'B3', DeliveryMethod.InPerson, Block.First, "Data Structures", 3))
-    testSection.append(Section([
-        ('M',10.25,11.25), ('W',10.25,11.25), ('F',10.25,11.25)
+    dataStructuresSections.append(Section([
+        ('T',14.5,16), ('R',14.5,16)
+    ],'CSE 336', 'B5', DeliveryMethod.InPerson, Block.First, "Data Structures", 3))
+    dataStructuresSections.append(Section([
+        ('T',9,10), ('R',9,10)
+    ],'CSE 336', 'B7', DeliveryMethod.InPerson, Block.First, "Data Structures", 3))
+    dataStructuresSections.append(Section([
+        ('T',10.25,11.25), ('R',10.25,11.25)
+    ],'CSE 336', 'B9', DeliveryMethod.InPerson, Block.First, "Data Structures", 3))
+
+    psycologySections.append(Section([],'PSY 101', 'A15', DeliveryMethod.Online, Block.Full, "Intro to Psycology", 3))
+    psycologySections.append(Section([
+        ('M',9,10.5), ('W',9,10.5), ('F',9,10.5)
     ],'PSY 101', 'A15', DeliveryMethod.InPerson, Block.Full, "Intro to Psycology", 3))
-    testSection.append(Section([
+    psycologySections.append(Section([
+        ('M',10.5,12), ('W',10.5,12), ('F',10.5,12)
+    ],'PSY 101', 'A15', DeliveryMethod.InPerson, Block.Full, "Intro to Psycology", 3))
+    psycologySections.append(Section([
+        ('M',14.5,16), ('W',14.5,16), ('F',14.5,16)
+    ],'PSY 101', 'A15', DeliveryMethod.InPerson, Block.Full, "Intro to Psycology", 3))
+
+    programmingWithClassesSections.append(Section([
         ('M',10.25,11.25), ('W',15,18), ('F',10.25,11.25)
     ],'CSE 220C', 'C4', DeliveryMethod.Flex, Block.Full, "Programming with Classes", 2))
 
-    for section in testSection:
-        print(section)
+    testSections = {}
+    testSections['C++ Language'] = cppSections
+    testSections['Data Structures'] = dataStructuresSections
+    testSections['Intro to Psycology'] = psycologySections
+    testSections['Programming with Classes'] = programmingWithClassesSections
+    
+    for course in testSections:
+        for section in testSections[course]: 
+            print(section)
 
 def main():
     test()
